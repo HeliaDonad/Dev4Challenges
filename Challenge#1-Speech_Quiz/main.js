062e41652fa183a8c710141f00c97aefb4487635
HeliaDonad <r0936750@student.thomasmore.be>
2024-05-05 22:27:36 +0200

    afbeeldingen

--- Challenge#1-Speech_Quiz/main.js
+++ Challenge#1-Speech_Quiz/main.js
@@ -30,23 +30,28 @@ let currentQuestionIndex = 0;
 const questions = [
   {
     question: "In welke Belgische stad bevindt zich het beroemde surrealistische kunstmuseum ontworpen door René Magritte?",
-    correctAnswer: "Brussel"
+    correctAnswer: "Brussel",
+    image: "images/magritte.jpg"
   },
   {
     question: "Wat is het beroemde standbeeld dat bekend staat als het symbool van Brussel en afgebeeld wordt als een plassend jongetje?",
-    correctAnswer: "Manneke Pis"
+    correctAnswer: "Manneken Pis",
+    image: "images/manneken-pis.jpg"
   },
   {
     question: "Welke Belgische stripheld heeft een hond genaamd Bobbie en beleeft spannende avonturen?",
-    correctAnswer: "Kuifje"
+    correctAnswer: "Kuifje",
+    image: "images/kuifje.png"
   },
   {
     question: "Hoe noem je het grote metalen monument in Brussel?",
-    correctAnswer: "Atomium"
+    correctAnswer: "Atomium",
+    image: "images/atomium.jpg"
   },
   {
-    question: "Wat is de naam van het Belgische bier dat bekend staat om zijn fruitige smaak en aroma?",
-    correctAnswer: "Kriek"
+    question: "Hoe heet de slechterik die altijd probeert om de Smurfen te vangen?",
+    correctAnswer: "Gargamel",
+    image: "images/gargamel.jpg"
   },
 ];
 
@@ -54,6 +59,8 @@ function readQuestion() {
   const question = questions[currentQuestionIndex];
   utterance.text = question.question;
   document.querySelector("#output").innerHTML = ""; // Leeg de output container
+  const imageContainer = document.querySelector("#image");
+  imageContainer.innerHTML = `<img src="${question.image}" alt="Afbeelding bij vraag">`;
   synth.speak(utterance);
 }
 
@@ -74,14 +81,11 @@ recognition.onresult = function(event) {
     antwoordBtn.disabled = false;
 };
 
-  
-
 antwoordBtn.addEventListener("click", function() {
     console.log("Antwoordknop geklikt...");
     recognition.start();
     antwoordBtn.disabled = true;
   });
-  
 
 nextBtn.addEventListener("click", function() {
   currentQuestionIndex++;
